3e408049e409ae0506609d743107f21b203ed993
Tosuccess <147279295+Powerctc@users.noreply.github.com>
2025-10-25 03:01:19 +0700

    Create AdBanner.jsx

---
+++ src/components/AdBanner.jsx
@@ -0,0 +1,16 @@
+export default function AdBanner() {
+  return (
+    <div className="adhere-gif text-center my-2 max-w-xs mx-auto border-2 border-yellow-400 rounded-md p-1 shadow">
+      <a href="https://aitools.tv" target="_blank" rel="noreferrer">
+        <img
+          src="https://djmorio.wordpress.com/wp-content/uploads/2020/04/place-your-advert-here-1.gif"
+          alt="Ad"
+          className="w-full rounded"
+        />
+      </a>
+      <p className="mt-1 text-yellow-400 font-bold text-sm">
+        ဘာလိုလို Visit now!
+      </p>
+    </div>
+  )
+}
